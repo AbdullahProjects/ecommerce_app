3b03ec42c0e109e8e0606dc289f46dc32991fd5f
Abdullah Khan Kakar <2022bbit71@student.uet.edu.pk>
2025-06-28 18:49:20 +0500

    configure firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:ecommerce_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/app.dart';
 
-void main() {
+Future<void> main() async {
+  // Todo: Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Load all the Material Design / Themes / Localizations / Bindings
   runApp(const App());
 }
